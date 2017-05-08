bfb8d8e5d4a8a3b8e974ab5cfecd49faaac9c882
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-05-08 06:53:25 +0000

    update login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
     
-root 'static_pages#help'
-
-root 'static_pages#about'
-
 root 'static_pages#home'
 
 get '/help', to: 'static_pages#help'
@@ -14,7 +10,8 @@ get '/contact', to: 'static_pages#contact'
 
 get  '/home', to:'static_pages#home'
 
-get '/about', to:'static_pages#about'
+get '/login', to:'static_pages#login'
 
+get '/sigh', to:'static_pages#sighup'
 
 end
